e127fe4979dc39515fd9429473b8659285ba9432
djodar <daniel.jodar@gmail.com>
2018-01-27 16:17:15 +0100

    First commit

---
+++ src/main/scala/datastructures/List.scala
@@ -0,0 +1,35 @@
+package datastructures
+
+sealed trait List[+A]
+
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+
+  def apply[A](as: A*): List[A] = // Variadic function syntax
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => sys.error("error")
+    case Cons(_, t) => t
+  }
+
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Cons(h, Nil)
+    case Cons(_, t) => Cons(h, t)
+  }
+
+  def drop[A](l: List[A], n: Int): List[A] = {
+    def dropAcc(l: List[A], n: Int, acc: Int): List[A] =
+      if(acc < n) {
+        dropAcc(tail(l), n, acc + 1)
+      } else {
+        l
+      }
+
+    dropAcc(l, n, 0)
+  }
+}
