20672d614e630d8c5552df3d7183dbe51bacd138
djodar <daniel.jodar@gmail.com>
2018-01-28 22:08:44 +0100

    Data structures

--- src/main/scala/datastructures/List.scala
+++ src/main/scala/datastructures/List.scala
@@ -1,5 +1,7 @@
 package datastructures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A]
 
 case object Nil extends List[Nothing]
@@ -7,6 +9,32 @@ case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
+
+  def sum(ints: List[Int]): Int =
+    ints match {
+      // A function that uses pattern matching to add up a list of integers
+      case Nil => 0 // The sum of the empty list is 0.
+      case Cons(x, xs) =>
+        x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h, t) => Cons(h, append(t, a2))
+    }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
 
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
@@ -24,7 +52,7 @@ object List {
 
   def drop[A](l: List[A], n: Int): List[A] = {
     def dropAcc(l: List[A], n: Int, acc: Int): List[A] =
-      if(acc < n) {
+      if (acc < n) {
         dropAcc(tail(l), n, acc + 1)
       } else {
         l
@@ -32,4 +60,72 @@ object List {
 
     dropAcc(l, n, 0)
   }
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h, t) if f(h) => dropWhile(t, f)
+      case Nil => l
+    }
+
+  def init[A](l: List[A]): List[A] = ???
+
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, acc) => acc + 1)
+
+  @tailrec
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Cons(head, tail) => foldLeft(tail, f(z, head))(f)
+    case Nil => z
+  }
+
+  def sum3(l: List[Int]): Int = foldLeft(l, 0)(_ + _)
+
+  def product3(l: List[Int]): Int = foldLeft(l, 1)(_ * _)
+
+  def length2[A](l: List[A]): Int = foldLeft(l, 0)((acc, _) => acc + 1)
+
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, List[A]())((acc, h) => Cons(h, acc))
+
+  def appendViaFoldRight[A](l: List[A], r: List[A]): List[A] =
+    foldRight(l, r)(Cons(_, _))
+
+  /*
+  Note that we're simply referencing the `append` function, without writing something like `(x,y) => append(x,y)`
+  or `append(_,_)`. In Scala there is a rather arbitrary distinction between functions defined as _methods_, which are
+  introduced with the `def` keyword, and function values, which are the first-class objects we can pass to other
+  functions, put in collections, and so on. This is a case where Scala lets us pretend the distinction doesn't exist.
+  In other cases, you'll be forced to write `append _` (to convert a `def` to a function value)
+  or even `(x: List[A], y: List[A]) => append(x,y)` if the function is polymorphic and the type arguments aren't known.
+   */
+  def concat[A](l: List[List[A]]): List[A] = foldLeft(l, List[A]())(append)
+
+  def add1(l: List[Int]): List[Int] =
+    foldRight(l, Nil: List[Int])((h, acc) => Cons(h + 1, acc))
+
+  def doubleToString(l: List[Double]): List[String] =
+    foldRight(l, Nil: List[String])((h, acc) => Cons(h.toString, acc))
+
+  def map[A, B](as: List[A])(f: A => B): List[B] =
+    foldRight(as, Nil: List[B])((h, acc) => Cons(f(h), acc))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] =
+    foldRight(as, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = concat(map(as)(f))
+
+  def filterWithFlatMap[A](l: List[A])(f: A => Boolean): List[A] =
+    flatMap(l)(a => if (f(a)) List(a) else Nil)
+
+  def addPairwise(a: List[Int], b: List[Int]): List[Int] = (a, b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairwise(t1, t2))
+  }
+
+  def zipWith[A, B, C](a: List[A], b: List[B])(f: (A, B) => C): List[C] =
+    (a, b) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+    }
 }
