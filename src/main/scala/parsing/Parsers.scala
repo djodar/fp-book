dd18a9e913aecaea201a45875a44c6a8375c3b91
djodar <daniel.jodar@gmail.com>
2018-05-29 14:15:50 +0200

    Temporary commit

--- src/main/scala/parsing/Parsers.scala
+++ src/main/scala/parsing/Parsers.scala
@@ -1,13 +1,29 @@
 package parsing
 
-import language.higherKinds
+import scala.language.higherKinds
 
 trait Parsers[Parser[+ _]] { self => // so inner classes may call methods of trait
 
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-  def char(c: Char): Parser[Char]
 
-  case class ParserOps[A](p: Parser[A]) {}
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(
+      implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+  def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
+
+  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+  def map[A, B](a: Parser[A])(f: A => B): Parser[B]
+  def many[A](p: Parser[A]): Parser[List[A]]
+
+  case class ParserOps[A](p: Parser[A]) {
+    def |[B >: A](p2: => Parser[B]): Parser[B] =
+      self.or(p, p2) // use `self` to explicitly disambiguate reference to the `or` method on the `trait`
+    def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+    def map[A, B](a: Parser[A])(f: A => B): Parser[B] = self.map(a)(f)
+    def many: Parser[List[A]] = self.many(p)
+  }
 
   object Laws {}
 }
@@ -30,3 +46,5 @@ case class Location(input: String, offset: Int = 0) {
 
 case class ParseError(stack: List[(Location, String)] = List(),
                       otherFailures: List[ParseError] = List()) {}
+
+object Parsers {}
